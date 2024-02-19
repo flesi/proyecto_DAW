a864182be9d408185397659e949fd19bd061f843
flesi <flesiking@gmail.com>
2024-02-19 18:53:45 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Proyecto DAW - Gestión Servicios'
+copyright = '2024, JLROMERO'
+author = 'José Luis Romero'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.0'
 
 # -- General configuration
 
